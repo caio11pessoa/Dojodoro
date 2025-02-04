6d721bbf47181f4e7bdc7d7883fb358491203d27
CaioPessoa <caiopessoa1298@gmail.com>
2025-02-04 12:32:05 -0300

    Initial Commit

---
+++ DojodoroTests/DojodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  DojodoroTests.swift
+//  DojodoroTests
+//
+//  Created by Caio de Almeida Pessoa on 04/02/25.
+//
+
+import Testing
+@testable import Dojodoro
+
+struct DojodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
