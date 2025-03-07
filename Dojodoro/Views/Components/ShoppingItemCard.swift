29db8c41d5f44d8119f9c48d0dacc289d1fd6b50
CaioPessoa <caiopessoa1298@gmail.com>
2025-03-07 17:48:42 -0300

    "Initialize `ShoppingItemCard` component file"

---
+++ Dojodoro/Views/Components/ShoppingItemCard.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingItemCard.swift
+//  Dojodoro
+//
+//  Created by Caio de Almeida Pessoa on 07/03/25.
+//
+
+import SwiftUI
+
+struct ShoppingItemCard: View {
+    var body: some View {
+        Text("Shopping Item Card")
+    }
+}
+
+#Preview {
+    ShoppingItemCard()
+}
