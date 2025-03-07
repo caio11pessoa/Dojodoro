8de40e1d54b832acf3365f71f9d8e10d4e5bf1da
CaioPessoa <caiopessoa1298@gmail.com>
2025-03-07 19:45:33 -0300

    Implement button component and add required assets

--- Dojodoro/Views/Components/ShoppingItemCard.swift
+++ Dojodoro/Views/Components/ShoppingItemCard.swift
@@ -7,12 +7,60 @@
 
 import SwiftUI
 
+
 struct ShoppingItemCard: View {
+    enum ShoppingItemImage {
+        case cherry, lakeWithShadow, darkSakuraShadow, rockComponentWithMusgo, twoRocksWithShadow
+    }
+    
+    let image: ShoppingItemImage
+    @State var isSelected: Bool = false
+    
+
     var body: some View {
-        Text("Shopping Item Card")
+        VStack {
+            ZStack {
+                cardBackground()
+                plantImage()
+            }
+            .onTapGesture {
+                withAnimation {
+                    isSelected.toggle()
+                }
+            }
+        }
+    }
+    
+    private func plantImage() -> some View {
+        switch image {
+        case .cherry:
+            return Image(.cherryShadow)
+        case .lakeWithShadow:
+            return Image(.lakeWithShadow)
+        case .darkSakuraShadow:
+            return Image(.darkSakuraShadow)
+        case .rockComponentWithMusgo:
+            return Image(.rockComponentWithMusgo)
+        case .twoRocksWithShadow:
+            return Image(.twoRocksWithShadow)
+        }
+    }
+    
+    private func cardBackground() -> some View {
+        isSelected ? Image(.emptyCardSelected)  : Image(.emptyCard)
     }
 }
 
 #Preview {
-    ShoppingItemCard()
+    VStack{
+        HStack{
+            ShoppingItemCard(image: .cherry, isSelected: false)
+            ShoppingItemCard(image: .lakeWithShadow, isSelected: false)
+        }
+        HStack {
+            ShoppingItemCard(image: .darkSakuraShadow, isSelected: false)
+            ShoppingItemCard(image: .rockComponentWithMusgo, isSelected: false)
+        }
+        ShoppingItemCard(image: .twoRocksWithShadow, isSelected: false)
+    }
 }
